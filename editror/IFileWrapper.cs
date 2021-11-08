696a3cb39900b0316ff7bc14875dc39aaf78cbd6
KateFedak <katefedakk@gmail.com>
2021-11-08 17:34:32 +0200

    /

---
+++ editror/IFileWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace editor.dll
+{
+  public  interface IFileWrapper
+    {
+        string ReadDataFromFile(string filePath);
+        bool CheckFileExists(string filePath);
+        void CopyToFile(string filePath, string destFile);
+        public void WriteInFile(string filePath, string information);
+    }
+}
---
+++ editror/IStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace editor.dll
+{
+  public  interface IStorage
+    {
+        void CopyFileToStorage(string filePath);
+        string[] GetFileNameInStorage();
+        int FindAndReplace(string fileName, string searchText, string replaceTet);
+        string[] SearchParagraphs(string fileName, string searchText);
+    }
+}
